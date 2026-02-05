f42fa72911563e03ccc805291cd1248db9434445
Justin Rhodes <justin.m.rhodes@gmail.com>
2026-02-05 16:42:36 -0600

    assets-preloaded

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preload from 'astro-preload';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
+
   vite: {
     envDir: '.',
   },
+
+  integrations: [preload()],
 });
